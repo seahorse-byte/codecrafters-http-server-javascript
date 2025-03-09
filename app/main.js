0c4c4324c71a9bd263a1f36ba424aa22fe9f0234
Olsi Gjeci <olsi.developer@gmail.com>
2025-03-09 23:44:26 +0000

    200 ok

--- app/main.js
+++ app/main.js
@@ -5,6 +5,18 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+// In this stage, your server will respond to an HTTP request with a 200 response.
+
+socket.on("data", (data) => {
+    const request = data.toString();
+    const response = "HTTP/1.1 200 OK\r\n\r\n";
+    socket.write(response);
+});
+
+
+
+
+
   socket.on("close", () => {
     socket.end();
   });
