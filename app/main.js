8cd72d5650388bbf3cd9498b03848e2df3a9515c
Olsi Gjeci <olsi.developer@gmail.com>
2025-03-09 23:49:31 +0000

    found not found

--- app/main.js
+++ app/main.js
@@ -5,20 +5,18 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-// In this stage, your server will respond to an HTTP request with a 200 response.
 
-socket.on("data", () => {
-    const response = "HTTP/1.1 200 OK\r\n\r\n";
-    socket.write(response);
-});
+    socket.on("data", (data) => {
+        const request = data.toString();
+        const path = request.split(" ")[1];
+        const response = path === "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+        socket.write(response);
+    });
 
-
-
-
-
-  socket.on("close", () => {
-    socket.end();
-  });
+    
+    socket.on("close", () => {
+        socket.end();
+    });
 });
 
 server.listen(4221, "localhost");
