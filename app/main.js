33d18d06f45cd56cff3c424e98df78b9e0cdad8f
Olsi Gjeci <olsi.gjeci@snyk.io>
2025-11-13 12:47:22 +0000

    read request body

--- app/main.js
+++ app/main.js
@@ -18,7 +18,10 @@ function parseHeaders(request) {
   return headers;
 }
 
-function handlePath(path, headers) {
+function handlePath(path, headers, method, body) {
+  console.log("headers", headers);
+  console.log("method", method);
+  console.log("body", body);
   if (path === "/") {
     return "HTTP/1.1 200 OK\r\n\r\n";
   } else if (path.startsWith("/echo/")) {
@@ -28,15 +31,26 @@ function handlePath(path, headers) {
     const userAgent = headers["User-Agent"] || "";
     return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
   } else if (path.startsWith("/files/")) {
-    try {
-      const filename = path.split("/")[2];
-      const filePath = `${directory}${filename}`;
-      const fileContent = fs.readFileSync(filePath); // No utf-8 for octet-stream
+    const filename = path.split("/")[2];
+    const filePath = `${directory}${filename}`;
+    if (method === "POST") {
+      // write the file with the post request body data passed with --data flag
+      try {
+        fs.writeFileSync(filePath, body);
 
-      return `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
-    } catch (error) {
-      // If the file doesn't exist, return 404
-      return "HTTP/1.1 404 Not Found\r\n\r\n";
+        return "HTTP/1.1 201 Created\r\n\r\n";
+      } catch (error) {
+        return "HTTP/1.1 500 Internal Server Error\r\n\r\n";
+      }
+    } else {
+      try {
+        const fileContent = fs.readFileSync(filePath); // No utf-8 for octet-stream
+
+        return `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${fileContent.length}\r\n\r\n${fileContent}`;
+      } catch (error) {
+        // If the file doesn't exist, return 404
+        return "HTTP/1.1 404 Not Found\r\n\r\n";
+      }
     }
   } else {
     return "HTTP/1.1 404 Not Found\r\n\r\n";
@@ -45,13 +59,14 @@ function handlePath(path, headers) {
 
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    // console.log(parseHeaders(data.toString()));
-    const request = data.toString();
-    const headers = parseHeaders(request);
-    const path = request.split(" ")[1];
+    // 1. Convert the data Buffer to a string
+    const requestString = data.toString();
+    const headers = parseHeaders(requestString);
+    const path = requestString.split(" ")[1];
+    const method = requestString.split(" ")[0];
+    const body = requestString.split("\r\n\r\n")[1];
 
-    const response = handlePath(path, headers);
-    console.log("response", response);
+    const response = handlePath(path, headers, method, body);
     socket.write(response);
     socket.end();
   });
