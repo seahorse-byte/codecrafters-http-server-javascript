20759fe44dde35c72bc660236a49bb4af1a0be5e
Olsi Gjeci <olsi.gjeci@snyk.io>
2025-11-13 11:20:19 +0000

    respond with body

--- app/main.js
+++ app/main.js
@@ -4,18 +4,32 @@ const net = require("net");
 console.log("Logs from your program will appear here!");
 
 const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    const request = data.toString();
+    const path = request.split(" ")[1];
+    console.log(path);
 
-    socket.on("data", (data) => {
-        const request = data.toString();
-        const path = request.split(" ")[1];
-        const response = path === "/" ? "HTTP/1.1 200 OK\r\n\r\n" : "HTTP/1.1 404 Not Found\r\n\r\n";
+    if (path === "/") {
+      const response = "HTTP/1.1 200 OK\r\n\r\n";
+      socket.write(response);
+    } else {
+      //    handle /echo/{str} where the body is the string str - res 200 with Content-Type and Content-Length header.
+      if (path.startsWith("/echo")) {
+        const str = path.split("/")[2];
+        const response = `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
         socket.write(response);
-    });
+      } else {
+        const response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        socket.write(response);
+      }
+    }
 
-    
-    socket.on("close", () => {
-        socket.end();
-    });
+    socket.end();
+  });
+
+  socket.on("close", () => {
+    socket.end();
+  });
 });
 
 server.listen(4221, "localhost");
